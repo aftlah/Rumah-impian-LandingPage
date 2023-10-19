2c11dcdd6beb5768b2dcb9167b6e4e560fbecd80
aftlah <altaf.fattah05@gmail.com>
2023-10-19 14:35:39 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const navbar = document.getElementsByTagName("nav")[0];
+
+window.addEventListener("scroll", () =>{
+    let scroll_position = window.scrollY
+console.log(scroll_position)
+
+  if(scroll_position > 695){
+    navbar.classList.replace('bg-transparent', 'nav-color')
+  }else if (scroll_position < 695){
+    navbar.classList.replace('nav-color', 'bg-transparent')
+  }
+});
