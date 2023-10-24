2c4ffbd47cd5320fb62ccc8ac319a980ea086792
unknown <altaf.fattah05@gmail.com>
2023-10-24 00:04:10 -0700

    Navbar Fix

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const navbar = document.getElementsByTagName("nav")[0];
 
-window.addEventListener("scroll", () =>{
-    let scroll_position = window.scrollY
-console.log(scroll_position)
+window.addEventListener("scroll", () => {
+  let scroll_position = window.scrollY;
+  console.log(scroll_position);
 
-  if(scroll_position > 560){
-    navbar.classList.replace('bg-transparent', 'nav-color')
-  }else if (scroll_position < 560){
-    navbar.classList.replace('nav-color', 'bg-transparent')
+  if (window.matchMedia("(max-width: 680px)")) {
+    navbar.classList.replace("bg-transparent", "nav-color");
+  } else {
+    if (scroll_position > 550) {
+      navbar.classList.replace("bg-transparent", "nav-color");
+    } else if (scroll_position < 550) {
+      navbar.classList.replace("nav-color", "bg-transparent");
+    }
   }
 });
